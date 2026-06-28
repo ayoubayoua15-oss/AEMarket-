d7d1239866f5648f59e5e8b436f8ce6869f4c417
ayoubayoua15-oss <ayoubayoua15@gmail.com>
2026-06-28 13:38:18 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1 style={{ fontSize: "40px" }}>AEMarket 🚀</h1>
+      <p>منصة بيع المنتجات الرقمية قيد التطوير</p>
+    </main>
+  );
+}
